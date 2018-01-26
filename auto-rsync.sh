82538b53c14c361f498079b09be9beded27443c0
4xdk <dabi@startmail.com>
2018-01-26 18:51:41 +0000

    Basic version, initial commit

---
+++ auto-rsync.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function runBackup {
+	if [ $# -eq 2 ]; then
+		if [ -d $1 ]; then
+			local loc_DST=$1;
+		else
+        	echo "Backup location $1 not found, exiting";
+			exit 1;
+		fi
+		
+	    if [ -f $2 ]; then
+            local loc_SRCLIST=$2            
+        else
+            echo "File $2 not found, exiting"
+			showHelp();
+            exit 1
+        fi
+
+		# TODO add confirmation/choice for potentially destructive --delete
+		while read DIR
+		do
+			echo Copying "$DIR" to "$loc_DST"
+        	rsync --delete --progress --times --recursive "$DIR" "$loc_DST"  
+		done < $loc_SRCLIST
+	else
+		echo "Missing argument(s)"
+		showHelp()
+		exit 1
+	fi
+}
+
+function showHelp {
+	echo "Usage: auto-rsync.sh backup-destination folder-list.txt"
+}
+
+runBackup
+
