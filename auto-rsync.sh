9a694824910d80926ed8545a175d82eb2dd80adf
4xdk <dabi@startmail.com>
2018-01-28 12:53:59 +0000

    rewrite to use getopts, first proper version

--- auto-rsync.sh
+++ auto-rsync.sh
@@ -1,39 +1,69 @@
 #!/bin/bash
 
-function runBackup {
-	if [ $# -eq 2 ]; then
-		if [ -d $1 ]; then
-			local loc_DST=$1
-		else
-        	echo "Backup location $1 not found, exiting"
-			showHelp
-			exit 1
-		fi
+function show_help {
+	echo "Usage: auto-rsync.sh -d backup-destination -i folder-list.txt"
+}
+
+# A POSIX variable
+OPTIND=1
+
+# Process parameters
+while getopts "h?vi:d:o:r" opt; do
+    case "$opt" in
+		d)  DESTINATION=$OPTARG
+    		;;
+		#o)  OUTPUT_FILE=$OPTARG
+        #	;;
+    	h)	show_help
+        	exit 0
+        	;;
+		i)  INPUT=$OPTARG
+			;;
+		r)  REMOVE=1
+			;;
+		v)  VERBOSE=1
+        	;;
+		\?) echo "Invalid option" >&2
+			;;
+    esac
+done
+
+shift $((OPTIND-1))
+[ "$1" = "--" ] && shift
+
+# check if provided input and destination exist
+if [ -z $DESTINATION ] || [ ! -d $DESTINATION ]; then
+	echo "Backup location not found, exiting"
+	show_help
+	exit 1
+fi
+
+if [ -z $INPUT ] || [ ! -f $INPUT ]; then
+    echo "File $INPUT not found, exiting"
+	show_help
+    exit 1
+fi
+
+# adjust rsync parameters
+PARAMS='--progress --times --recursive'
+if [ $REMOVE ]; then
+	PARAMS="--delete $PARAMS"
+fi
+
+#if [ ! -z $OUTPUT_FILE ]; then
+#	OUTPUT_PARAM="1>>$OUTPUT_FILE 2>>$OUTPUT_FILE.errors"
+#fi
+
+function run_backup {
+	echo "using $INPUT file to backup to $DESTINATION"
 		
-	    if [ -f $2 ]; then
-            local loc_SRCLIST=$2            
-        else
-            echo "File $2 not found, exiting"
-			showHelp
-            exit 1
-        fi
-
-		# TODO add confirmation/choice for potentially destructive --delete
-		while read DIR
-		do
-			echo Copying "$DIR" to "$loc_DST"
-        	rsync --delete --progress --times --recursive "$DIR" "$loc_DST"  
-		done < $loc_SRCLIST
-	else
-		echo "Missing argument(s)"
-		showHelp
-		exit 1
-	fi
+	while read DIR
+	do
+		FOLDER=$( rev <<< $DIR | cut -d/ -f2 | rev )
+		echo Copying "$DIR" to "$DESTINATION$FOLDER"
+		rsync $PARAMS "$DIR" "$DESTINATION$FOLDER" -q 
+	done < $INPUT
 }
 
-function showHelp {
-	echo "Usage: auto-rsync.sh backup-destination folder-list.txt"
-}
-
-runBackup
+run_backup 
 
